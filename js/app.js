520aa54ca22966b14d94ff4aa1bf30c297bbf77d
issansabimana <issa.nsabimana@codeland.it>
2022-01-26 21:29:32 +0200

    adding validation

---
+++ js/app.js
@@ -0,0 +1,80 @@
+const inputName = document.querySelector('#name');
+const sendBtn = document.querySelector('.sendBtn');
+const resetBtn = document.querySelector('.resetBtn');
+const textArea = document.querySelector('.msg');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const company = document.querySelector('#company');
+const nameLabel = document.querySelector('#nameLabel');
+const emailLabel = document.querySelector('#emailLabel');
+const phoneLabel = document.querySelector('#phoneLabel');
+const companyLabel = document.querySelector('#companyLabel');
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+phone.addEventListener('keypress', (e) => {
+    if (!phone.value.match(/^\d{7,9}$/)) {
+        phone.style.borderColor = 'red';
+        phoneLabel.innerHTML = 'Phone number must have between 8 and 10 digits';
+        phoneLabel.style.color = 'red';
+    }
+});
+
+const validator =((inputItem, labelName,errorMsg)=>{
+    if (inputItem.value.length < 1) {
+        labelName.innerHTML = errorMsg;
+        inputItem.style.borderColor = 'red';
+        labelName.style.color = 'red';
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (email.value.length < 1 || !email.value.match(emailRegex)){
+        emailLabel.innerHTML = 'Write an email(example@example.com)'
+        email.style.borderColor = 'red';
+        emailLabel.style.color = 'red';
+    }
+    validator(inputName,nameLabel,'Write your name and surname')
+    validator(phone,phoneLabel,'Write your phone number')
+    validator(company,companyLabel,'Write your company name')
+    validator(textArea,textArea,'Write your message')
+
+    if(nameLabel.style.color !=="red" && emailLabel.style.color!=="red" && companyLabel.style.color!=="red" && phoneLabel.style.color!=="red" && textArea.style.color!=="red"){
+    document.querySelector(".loaderContainer").classList.remove("hidden")
+    }
+});
+
+resetBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    document.querySelectorAll('input').forEach((input) => {
+        input.style.borderColor = 'black';
+        textArea.style.borderColor = 'black';
+        textArea.style.color = 'black';
+        input.value = '';
+        textArea.value = ''
+    });
+    document.querySelectorAll('label').forEach((label) => label.style.color = 'black')
+    nameLabel.innerHTML = 'Name';
+    emailLabel.innerHTML = 'Email';
+    phoneLabel.innerHTML = 'Phone';
+    companyLabel.innerHTML = 'Company';
+    textArea.style.borderColor = 'black';
+    textArea.style.color = 'black';
+    textArea.innerHTML= '';
+});
+
+const removeError = (inputName, labelName) => {
+    const actualLabel = labelName.innerHTML
+    inputName.addEventListener('keydown', () => {
+        inputName.style.borderColor =  'black';
+        labelName.innerHTML = actualLabel;
+       labelName.style.color =  'black';
+    })
+}
+
+removeError(inputName, nameLabel)
+removeError(email, emailLabel)
+removeError(phone, phoneLabel)
+removeError(company, companyLabel)
+removeError(textArea, textArea)
